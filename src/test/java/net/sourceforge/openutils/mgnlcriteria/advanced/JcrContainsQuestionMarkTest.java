09825abb1595aa8cf0b62f4d2ea3c8d0a2eb56e1
diego_schivo <diego_schivo@8cde5574-a10d-0410-8942-c3fc08a91110>
2012-03-21 16:18:34 +0000

    JcrContainsQuestionMarkTest: fix NullPointerException
    git-svn-id: https://svn.code.sf.net/p/openutils/code/magnoliamodules/trunk@3886 8cde5574-a10d-0410-8942-c3fc08a91110

--- src/test/java/net/sourceforge/openutils/mgnlcriteria/advanced/JcrContainsQuestionMarkTest.java
+++ src/test/java/net/sourceforge/openutils/mgnlcriteria/advanced/JcrContainsQuestionMarkTest.java
@@ -20,8 +20,11 @@
 package net.sourceforge.openutils.mgnlcriteria.advanced;
 
 import info.magnolia.cms.core.HierarchyManager;
+import info.magnolia.cms.i18n.DefaultI18nContentSupport;
+import info.magnolia.cms.i18n.I18nContentSupport;
 import info.magnolia.context.MgnlContext;
 import info.magnolia.repository.RepositoryConstants;
+import info.magnolia.test.ComponentsTestUtil;
 import it.openutils.mgnlutils.test.RepositoryTestConfiguration;
 import it.openutils.mgnlutils.test.TestNgRepositoryTestcase;
 
@@ -65,6 +68,8 @@ public class JcrContainsQuestionMarkTest extends TestNgRepositoryTestcase
         // - dolor sit
         // - dolor sit amet
         MgnlContext.getHierarchyManager(RepositoryConstants.WEBSITE).save();
+
+        ComponentsTestUtil.setInstance(I18nContentSupport.class, new DefaultI18nContentSupport());
     }
 
     @SuppressWarnings("deprecation")
