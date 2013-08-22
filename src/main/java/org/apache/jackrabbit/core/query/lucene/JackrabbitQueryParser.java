6802d7f2d008d84bb1954466df384e05dee02162
lucaboati <lucaboati@8cde5574-a10d-0410-8942-c3fc08a91110>
2013-08-22 11:06:56 +0000

    license format
    git-svn-id: https://svn.code.sf.net/p/openutils/code/magnoliamodules/trunk@4282 8cde5574-a10d-0410-8942-c3fc08a91110

--- src/main/java/org/apache/jackrabbit/core/query/lucene/JackrabbitQueryParser.java
+++ src/main/java/org/apache/jackrabbit/core/query/lucene/JackrabbitQueryParser.java
@@ -1,3 +1,22 @@
+/**
+ *
+ * Criteria API for Magnolia CMS (http://www.openmindlab.com/lab/products/mgnlcriteria.html)
+ * Copyright(C) 2009-2013, Openmind S.r.l. http://www.openmindonline.it
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // Included patch from: https://issues.apache.org/jira/browse/JCR-2732
 
 /*
