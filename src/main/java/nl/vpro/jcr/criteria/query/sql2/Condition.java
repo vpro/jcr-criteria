9be70db19de2bf31e709387be8273da6805e77a2
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-07-29 15:50:59 +0200

    javadoc.

--- src/main/java/nl/vpro/jcr/criteria/query/sql2/Condition.java
+++ src/main/java/nl/vpro/jcr/criteria/query/sql2/Condition.java
@@ -7,6 +7,10 @@ package nl.vpro.jcr.criteria.query.sql2;
  */
 public interface Condition {
 
+
+    /**
+     * return if something appended
+     */
     boolean toSql2(StringBuilder builder);
 
 
