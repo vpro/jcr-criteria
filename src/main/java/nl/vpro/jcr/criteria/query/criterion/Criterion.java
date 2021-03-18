241ae1f88e86b35c8d7ab1128169cb44e2478c90
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2021-03-18 12:11:10 +0100

    clarified exceptions a bit

--- src/main/java/nl/vpro/jcr/criteria/query/criterion/Criterion.java
+++ src/main/java/nl/vpro/jcr/criteria/query/criterion/Criterion.java
@@ -85,15 +85,14 @@ public interface Criterion extends Serializable {
      */
     @Deprecated
     default String toXPathString(Criteria criteria) throws JCRQueryException {
-        throw new UnsupportedOperationException("" + getClass().getName() + " does not support XPATH");
-
+        throw new UnsupportedOperationException("" + getClass().getName() + " does not support XPATH (requested for " + criteria + ")");
     }
 
     /**
      * @since 2.0
      */
     default Condition toSQLCondition(Criteria criteria) {
-        throw new UnsupportedOperationException("" + getClass().getName() + " does not yet support SQL2");
+        throw new UnsupportedOperationException("" + getClass().getName() + " does not yet support SQL2 (requested for " + criteria + ")");
     }
 
 }
